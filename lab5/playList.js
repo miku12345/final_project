c2999cbd90036a19b68d83875e999a1511163797
Feather_Futa <the.first.song12345@gmail.com>
2023-03-29 00:53:39 +0800

    first commit

---
+++ lab5/playList.js
@@ -0,0 +1,14 @@
+//YouTube Video ID
+let playList=[
+    "p2OpzDkVoQ8",
+    "s1vf4OuN9Os",
+    "Q_oag_LmLQA",
+    "t8CG9BdzzgM"
+];
+//播放起訖秒數
+let playTime=[
+    [74,84],
+    [0,5],
+    [18,26],
+    [176,178],
+];
